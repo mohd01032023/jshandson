4808e8a2a39f6c29b37e2ff2ec914abd98c10b38
shuaibcedcoss <126545931+mohd01032023@users.noreply.github.com>
2023-03-13 13:24:25 +0530

    Update handson.js

--- handson.js
+++ handson.js
@@ -1,4 +1,1 @@
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
-fact = "is awesome!";
-console.log(FCC, fact); // Change this line
+const remainder = 11%3;
